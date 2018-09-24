3dc4a9a62ba01b5ab4df8f082d1cc4b0af90bf61
Mikael Ragnhult <mikaelragnhult@spotify.com>
2018-09-24 11:32:38 +0200

    Empty project

---
+++ app/src/main/java/se/evinja/rxandroid/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package se.evinja.rxandroid.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import se.evinja.rxandroid.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
