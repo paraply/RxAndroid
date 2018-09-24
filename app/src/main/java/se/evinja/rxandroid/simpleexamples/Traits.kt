c24acc4c8531328ecec37887b820db4ffe2c0102
Mikael Ragnhult <mikaelragnhult@spotify.com>
2018-09-24 15:13:28 +0200

    SimpleRx traits

--- app/src/main/java/se/evinja/rxandroid/simpleexamples/Traits.kt
+++ app/src/main/java/se/evinja/rxandroid/simpleexamples/Traits.kt
@@ -1,5 +1,87 @@
 package se.evinja.rxandroid.simpleexamples
 
-/**
- * New Class.
- */
+import io.reactivex.Completable
+import io.reactivex.Maybe
+import io.reactivex.Single
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+
+object TraitsRx {
+    var bag = CompositeDisposable()
+
+    fun traits_single() {
+        val single = Single.create<String> { single ->
+            //do some logic here
+            val success = true
+
+
+            if (success) { //return a value
+                single.onSuccess("nice work!")
+            } else {
+                val someException = IllegalArgumentException("some fake error")
+                single.onError(someException)
+            }
+        }
+
+        single.subscribe({ result ->
+            //do something with result
+            println("👻 single: ${ result }")
+        }, { error ->
+            //do something for error
+        }).addTo(bag)
+    }
+
+
+    fun traits_completable() {
+
+        val completable = Completable.create { completable ->
+            //do logic here
+            val success = true
+
+            if (success) {
+                completable.onComplete()
+            } else {
+                val someException = IllegalArgumentException("some fake error")
+                completable.onError(someException)
+            }
+        }
+
+        completable.subscribe({
+            //handle on complete
+            println("👻 Completable completed")
+        }, { error ->
+            //do something for error
+        }).addTo(bag)
+
+    }
+
+    fun traits_maybe() {
+        val maybe = Maybe.create<String> { maybe ->
+            //do something
+            val success = true
+            val hasResult = true
+
+
+            if (success) {
+                if (hasResult) {
+                    maybe.onSuccess("some result")
+                } else {
+                    maybe.onComplete()
+                }
+            } else {
+                val someException = IllegalArgumentException("some fake error")
+                maybe.onError(someException)
+            }
+        }
+
+        maybe.subscribe({ result ->
+            //do something with result
+            println("👻 Maybe - result: ${ result }")
+        }, { error ->
+            //do something with the error
+        }, {
+            //do something about completing
+            println("👻 Maybe - completed")
+        }).addTo(bag)
+    }
+}
